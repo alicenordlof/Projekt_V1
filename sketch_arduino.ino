69cbf7cff87edf26108bf2ec18546ab2f447b9a9
alicenordlof <alicenordlof0744@gmail.com>
2025-04-29 12:09:59 +0200

    Update sketch_arduino.ino
    Detta är försök 1

--- sketch_arduino.ino
+++ sketch_arduino.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+// CHATGPT
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
 
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+// SLUT HJÄLP
+
+int val = 0;
+int distanceThreshold = 100;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(8, OUTPUT);  // Grön lampa
+  pinMode(9, OUTPUT);  // Röd lampa
+  pinMode(10, OUTPUT); // Relä
+  digitalWrite(10, LOW); // Reläet initialt avstängt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  val = vl.readRange();
 
+  delay(100);
+
+  if (val < distanceThreshold) {
+    digitalWrite(8, HIGH);   // Grön lampa på
+    digitalWrite(9, LOW);    // Röd lampa av
+    digitalWrite(10, HIGH);  // Relä på
+  } else {
+    digitalWrite(8, LOW);    // Grön lampa av
+    digitalWrite(9, HIGH);   // Röd lampa på
+    digitalWrite(10, LOW);   // Relä av
+  }
 }
